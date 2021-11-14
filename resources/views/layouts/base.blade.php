95acb72f7060c84c1c79f067d38d915d1ec6ba84
Eugene Grava <eugenegrava.c@gmail.com>
2021-11-14 20:28:56 +0800

    Updated footer
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,7 +20,7 @@
 
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; East Side Shoppe</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
