a1cc17de9d31b74f802be810ea72c320bcb9c4c3
Eugene Grava <eugenegrava.c@gmail.com>
2021-11-14 16:37:35 +0800

    Updated Product Model
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $table        = 'products';
+    protected $primaryKey   = 'product_id';
+    protected $fillable     = ['name', 'price', 'quantity'];
 }
