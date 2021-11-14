2d5380427c74b2b95cd56422c50cf5bbd75a7f60
Eugene Grava <eugenegrava.c@gmail.com>
2021-11-14 16:24:02 +0800

    Created migration for products
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

---
+++ database/migrations/2021_11_14_081456_product.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
