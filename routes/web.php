c009945fbabad705b1773da6dac5ca10f5c20528
Eugene Grava <eugenegrava.c@gmail.com>
2021-11-14 18:32:01 +0800

    Updated shop index and route
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'ShopController@index')->name('get.shop');
 
 Route::resource('product', 'ProductController');
+Route::post('item', 'OrderItemController@store')->name('item.store');
